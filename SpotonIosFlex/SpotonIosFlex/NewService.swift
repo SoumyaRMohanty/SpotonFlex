2a58759fba42f982e4146b86897f75be56180462
SoumyaMohanty <smohanty@spoton.com>
2021-04-21 16:49:48 +0530

    podspec file

---
+++ SpotonIosFlex/SpotonIosFlex/NewService.swift
@@ -0,0 +1,16 @@
+//
+//  NewService.swift
+//  SpotonIosFlex
+//
+//  Created by Soumya Mohanty on 4/21/21.
+//
+
+import Foundation
+
+public class NewService {
+    private init() {}
+    
+    public static func doSomething() -> String {
+        return "Soumya Test Framework Done"
+    }
+}
