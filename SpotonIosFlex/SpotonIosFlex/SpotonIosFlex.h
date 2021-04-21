1969c3fb971c3cef24e34fb274f84704679d7ea4
SoumyaMohanty <smohanty@spoton.com>
2021-04-21 16:37:32 +0530

    Project setup

---
+++ SpotonIosFlex/SpotonIosFlex/SpotonIosFlex.h
@@ -0,0 +1,18 @@
+//
+//  SpotonIosFlex.h
+//  SpotonIosFlex
+//
+//  Created by Soumya Mohanty on 4/21/21.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for SpotonIosFlex.
+FOUNDATION_EXPORT double SpotonIosFlexVersionNumber;
+
+//! Project version string for SpotonIosFlex.
+FOUNDATION_EXPORT const unsigned char SpotonIosFlexVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SpotonIosFlex/PublicHeader.h>
+
+
